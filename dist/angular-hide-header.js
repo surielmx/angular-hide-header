b3b2f413215c977e61fb52e2964fb73c1017b510
pokaxperia <pokazzam@gmail.com>
2015-02-12 17:22:52 -0600

    Update angular-hide-header.js
    fix cs transitions and sentence if and else reduced

--- dist/angular-hide-header.js
+++ dist/angular-hide-header.js
@@ -13,26 +13,19 @@
 					var pxOffset = parseInt(hideOffset);
 					$timeout.cancel(scroll_time);
 					if(current_scroll >= hheight+pxOffset){
-						if (current_scroll <= scrollposition) {
-					 		element.removeClass('hideh');
+						if (current_scroll < scrollposition) {
 							element.css({'top':"0px"});
 						}
-						else{
-					  		element.addClass('hideh');
+						else if (current_scroll > scrollposition) {
 							element.css({
 								'top':-hheight+"px",
-								'transition':'top 0.25s',
-								'-webkit-transition': 'top 0.25s',
-								'-moz-transition': 'top 0.25s',
-								'-ms-transition': 'top 0.25s',
-								'-o-transition': 'top 0.25s',
+								'transition':'all 0.25s',
+								'-webkit-transition': 'all 0.25s',
+								'-moz-transition': 'all 0.25s',
+								'-ms-transition': 'all 0.25s',
+								'-o-transition': 'all 0.25s',
 							});
 						}
-					}
-					else if(current_scroll >= hheight){
-						element.removeClass('hideh');
-						element.css({
-							'top':"0px"});
 					}
 					scroll_time = $timeout(function(){
 						scrollposition = body[0].scrollTop;
